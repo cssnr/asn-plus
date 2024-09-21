196d29d007c47b1e3a53f0cf19cf49dd0fad59f6
Shane <6071159+smashedr@users.noreply.github.com>
2024-09-21 14:04:14 -0700

    Fix setUninstallURL

--- src/js/service-worker.js
+++ src/js/service-worker.js
@@ -218,7 +218,6 @@ function setUninstallURL() {
     url.searchParams.append('version', manifest.version)
     chrome.runtime.setUninstallURL(url.href)
     console.debug(`setUninstallURL: ${url.href}`)
-    chrome.runtime.setUninstallURL(url.href)
 }
 
 /**
